4d5dfc8d051f43ec0398a3e77080aa07b8072d4a
IUT GEII Toulon Table 6 <qsdqsdq>
2020-10-14 13:27:17 +0200

    Add Automatical Connection to Serial + Event Test (10%)

--- RobotConsole/RobotConsole/Program.cs
+++ RobotConsole/RobotConsole/Program.cs
@@ -2,46 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ExtendedSerialPort;
 using System.Management;
+using System.IO.Ports;
 
 
 namespace RobotConsole
 {
     class Program
     {
-        ReliableSerialPort serialPort;
+        static ReliableSerialPort serialPort;
         
         static CMDGui cmdGui;
         static MsgEncoder msgEncoder;
         static MsgDecoder msgDecoder;
+        static SerialEvent serialEvent;
         
         static void Main(string[] args)
         {
-            ConsoleTitleFormat("MAIN", "Begin Booting Sequence", true);
+            ConsoleInformationFormat("MAIN", "Begin Booting Sequence", true);
             msgDecoder = new MsgDecoder();
             msgEncoder = new MsgEncoder();
+            serialEvent = new SerialEvent();
             cmdGui = new CMDGui();
-            GetSerialPort();
-            ConsoleTitleFormat("MAIN", "End  Booting Sequence", true);
+            ushort i = 0;
+            do
+            {
+                i++;
+                ConsoleInformationFormat("SERIAL", "Attempt Connection #" + i, true);
+                string AvailableCOM = GetSerialPort();
+
+                if (AvailableCOM != "") {
+                    ConsoleInformationFormat("SERIAL", "Available Serial: " + AvailableCOM , true);
+                    serialPort = new ReliableSerialPort(AvailableCOM, 115200, Parity.None, 8, StopBits.One);
+                    serialPort.DataReceived += serialEvent.SerialPort_DataReceived;
+                    serialEvent.OnSerialConnected(new SerialEventArgs(AvailableCOM));
+                    serialPort.Open();
+                    
+                } else
+                {
+                    ConsoleInformationFormat("SERIAL", "No Connection Available", false);
+                }
+                System.Threading.Thread.Sleep(1000);
+            } while (serialPort == null && i <= 255);
+            
+            ConsoleInformationFormat("MAIN", "End  Booting Sequence", true);
             Console.ReadKey();
             //cmdGui.InitializeCMDGui(); 
         }
 
-        static public void ConsoleTitleFormat(string title, string content, bool isCorrect)
+        
+
+        
+
+        static public void ConsoleTitleFormat (string title, bool isCorrect)
         {
             Console.Write("[");
             if (isCorrect)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-            } else
+            }
+            else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
             }
             Console.Write(title);
             Console.ResetColor();
-            Console.WriteLine("] " + content);
+            Console.Write("] ");
+        }
+        static public void ConsoleInformationFormat(string title, string content, bool isCorrect)
+        {
+            ConsoleTitleFormat(title, isCorrect);
+            Console.WriteLine(content);
         }
 
         static public void ConsoleListFormat(string content, ConsoleColor color)
@@ -50,12 +84,13 @@ namespace RobotConsole
             Console.WriteLine("    - " + content);
         }
 
-        private static void GetSerialPort()
+        private static string GetSerialPort()
         {
             try
             {
                 ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity");
-                ConsoleTitleFormat("SERIAL", "List of Serial available:", true);
+                ConsoleInformationFormat("SERIAL", "List of Serial available:", true);
+                string AvailableCOM = "";
                 foreach (ManagementObject queryObj in searcher.Get())
                 {
                     if (queryObj != null && queryObj["Caption"] != null)
@@ -65,26 +100,25 @@ namespace RobotConsole
                             string queryString = queryObj["Caption"].ToString();
                             string[] queryStringArray = queryString.Split(' ');
                             string COM_Name = queryStringArray[queryStringArray.Length - 1]; // Get (COMx)
-                            string COM_Description = queryString.Remove(queryString.IndexOf(COM_Name));
-                            COM_Name = COM_Name.Remove(COM_Name.Length - 1).Remove(1, 1); // Remove ( ) From COM
-                            ConsoleColor foreground_color;
+                            string COM_Description = queryString.Remove(queryString.IndexOf(COM_Name) - 1);
+                            COM_Name = COM_Name.Remove(COM_Name.Length - 1).Remove(0, 1); // Remove ( ) From COM
+                            ConsoleColor foreground_color = ConsoleColor.DarkGray;
                             if (COM_Description == "USB Serial Port")
                             {
+                                AvailableCOM = COM_Name;
                                 foreground_color = ConsoleColor.DarkGreen;
-                            }
-                            else
-                            {
-                                foreground_color = ConsoleColor.Gray;
                             }
                             ConsoleListFormat(COM_Name, foreground_color);
                         }
                     }
                 }
                 Console.ResetColor();
+                return (AvailableCOM);
             }
             catch (ManagementException)
             {
-                ConsoleTitleFormat("SERIAL", "ERROR while getting descritption", false);
+                ConsoleInformationFormat("SERIAL", "ERROR while getting descritption", false);
+                return ""; 
             }
         }
     }
---
+++ RobotConsole/RobotConsole/SerialEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtendedSerialPort;
+
+namespace RobotConsole
+{
+    class SerialEvent
+    {
+        static Queue<byte> byteListReceived = new Queue<byte>();
+        public void OnSerialConnected(SerialEventArgs e)
+        {
+            Program.ConsoleInformationFormat("SERIAL", "Connection Enabled: " + e.COM, true);
+        }
+
+        public void SerialPort_DataReceived(object sender, DataReceivedArgs e)
+        {
+            for (int i = 0; i < e.Data.Length; i++)
+            {
+                byteListReceived.Enqueue(e.Data[i]);
+            }
+        }
+    }
+    
+
+    class SerialEventArgs : EventArgs
+    {
+        public string COM { get; set; }
+
+        public SerialEventArgs(string COM_a)
+        {
+            COM = COM_a;
+        }
+    }
+}
