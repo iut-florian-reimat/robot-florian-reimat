71a0277565e6f40025b0e048576d9598cfb72563
IUT GEII Toulon Table 6 <qsdqsdq>
2021-02-05 17:37:24 +0100

    Add Init of Trajectory Planner

---
+++ Library/TrajectoryPlanner/TrajectoryPlanner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Utilities;
+
+namespace TrajectoryPlanner
+{
+    
+
+    public class TrajectoryPlanner
+    {
+        uint accelerationLinearMax;
+        uint accelerationAngularMax;
+
+        uint speedLinearMax;
+        uint speedAngularMax;
+
+        Location robotLocation;
+        Location robotDestination;
+
+        public TrajectoryPlanner()
+        {
+
+        }
+    }
+}
